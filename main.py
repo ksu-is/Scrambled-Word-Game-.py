c53490c88bf11a46b91c21a97892ca11eee950e8
MakennaHogan <156006903+MakennaHogan@users.noreply.github.com>
2024-04-08 10:19:28 -0400

    Update main.py
    Starting to write into code and creating the game.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-this is where we will complete our code for our project
+import random
+
+# This is the welcome screen code
+def welcome():
+    print("Welcome to the Word Scrambled Game!")
+    print("There are three levels, to move through each level you will have to unscramble each word correctly.")
+    print("Enjoy unscrambling!")
+  input("Press Enter to Start!")
+print()
+
+# This is the word bank 
+words = ['fun', 'alert', 'computer']
+
+
+for word in range(len(words)):
+  scrambled =  "".join(random.sample(words[word] , len(words[word])))
+  print (f"Unscramble this word! {scrambled} ")
+ guess = input("Write here: ")
+if guess == words[word]:
+  print("Correct! Move on to the next level!")
+else:
+  print("Incorrect, Please try again!")
+ 
+
